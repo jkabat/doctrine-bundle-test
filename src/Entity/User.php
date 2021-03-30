f4e61a525b711cb54b3a6f00c5f3ff94ba67600a
Juraj Kabát <kabat.juraj@gmail.com>
2021-03-30 14:05:17 +0200

    initial commit

---
+++ src/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use MsgPhp\User\Model\EmailPasswordCredential;
+use MsgPhp\User\User as BaseUser;
+use MsgPhp\User\UserId;
+
+/**
+ * @ORM\Entity()
+ */
+class User extends BaseUser
+{
+    use EmailPasswordCredential;
+
+    /** @ORM\Id() @ORM\GeneratedValue() @ORM\Column(type="msgphp_user_id", length=191) */
+    private $id;
+
+    public function __construct(UserId $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): UserId
+    {
+        return $this->id;
+    }
+}
